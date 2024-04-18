686f68789d39844aa1a37c7125962e6234ff318a
Leonel Medina <123245262+leomed98@users.noreply.github.com>
2024-04-17 20:55:11 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 
+//this func is used to track user input
 void playerMovement(char board[3][3], int row, int col, char currentPlayer){
     board[row][col]= currentPlayer;
 }
 
 bool checkWinner(char board [3][3], char currentPlayer){
-    
+    for( int i =0; i< 3; i++){ //iterator
+
+    if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] ==currentPlayer)
+    return true;
+    //this is to check for a horizontal win 
+    if (board[0][i] == currentPlayer && board [1][i] == currentPlayer && board[2][i] == currentPlayer)
+    return true;
+    //this is to check a vertical win
+
+    }
+
+    //now to check for diagonals
+    if(board[0][0] == currentPlayer && board[1][1] ==currentPlayer && board[2][2]==currentPlayer)
+    return true;
+    //this checks a win from top left to bottom right
+
+    if(board[0][2] == currentPlayer&& board[1][1] ==currentPlayer && board[2][0] ==currentPlayer)
+    return true;
+    //this is for bottom left to top right 
+
+    return false;
+    //this is for no wins
 }
+
+
+
+
+
+    
+
 
 int main() {
     char map[3][3] = {
@@ -15,9 +44,9 @@ int main() {
         {'  ', '  ', '  '},
     };
 
-   const char Player1 = 'X';
+   const char player1 = 'X';
     const char player2 = 'O';
-    char currentPlayer= Player1;
+    char currentPlayer= player1 ;
     int row, col;
 
 
@@ -32,8 +61,17 @@ std::cout << "Board:" << std::endl;
         std::cout << "|" << std::endl;
     }
 
-    std::cout << "Player 1" << currentPlayer <<" enter desired row/column (0-2)";
+    std::cout << "Player " << currentPlayer <<" enter desired row (0-2)";
+    std::cin>>row;
+
+    std::cout << "player " << currentPlayer << " enter desired column (0-2)";
+    std::cin>>col;
     
+    if (row > 2 || row < 0 || col > 2 || col < 0){
+        std::cout<<"Invalid move, try again" << std::endl;
+        --turn; //this lets user try again in the same turn
+
+    }
 
 
 }
